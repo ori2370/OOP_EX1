33a6fe3c131a18ec1a1fb3080eb8014c19e043b2
ori2370 <ori23700@gmail.com>
2022-12-27 15:08:58 +0200

    .

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,10 +1,18 @@
 package observer;
 
-public class ConcreteMember implements Member{
+public class ConcreteMember implements Member {
     private UndoableStringBuilder usb;
+    private final String name;
+
+    public ConcreteMember(UndoableStringBuilder usb, String name) {
+        this.usb = usb;
+        this.name = name;
+    }
+
     @Override
     public void update(UndoableStringBuilder usb) {
         this.usb = usb;
+        System.out.println("Member " + name + ": " + usb);
     }
 
 }
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -1,45 +1,76 @@
 package observer;
+
 import java.util.ArrayList;
-/** */
+
+/**
+ *
+ */
 public class GroupAdmin implements Sender {
-    private ArrayList<Member> clients= new ArrayList<>();
+    private ArrayList<Member> clients = new ArrayList<>();
     private UndoableStringBuilder usb;
 
+    /**
+     * @param obj
+     */
     @Override
     public void register(Member obj) {
-    this.clients.add(obj);
+        this.clients.add(obj);
     }
 
+    /**
+     * @param obj
+     */
     @Override
     public void unregister(Member obj) {
         this.clients.remove(obj);
     }
 
+    /**
+     * @param offset
+     * @param obj
+     */
     @Override
     public void insert(int offset, String obj) {
-    this.usb= usb.insert(offset,obj);
+        this.usb = usb.insert(offset, obj);
         notifyObservers();
     }
 
+    /**
+     * Appends the specified string to this character sequence and updates it to
+     * our clients database (Observer)
+     *
+     * @param obj the {@code StringBuffer} to append.
+     */
     @Override
     public void append(String obj) {
-    this.usb=usb.append(obj);
+        this.usb = usb.append(obj);
         notifyObservers();
     }
 
+    /**
+     * @param start
+     * @param end
+     */
     @Override
     public void delete(int start, int end) {
-    this.usb=usb.delete(start, end);
+        this.usb = usb.delete(start, end);
         notifyObservers();
     }
 
+    /**
+     *
+     */
     @Override
     public void undo() {
-    this.usb.undo();
+        this.usb.undo();
         notifyObservers();
     }
-    public void notifyObservers(){
-        for(Member member: clients){
+
+    /**
+     *
+     */
+    public void notifyObservers() {
+        for (Member member : clients) {
             member.update(usb);
         }
     }
